e69bbb962445110f520accd9c4223dcfc3bf3b6e
Edith <Saikiran.Vodela@fleetcor.com>
2024-02-18 19:00:14 +0530

    stable v2

--- LoRaSender/LoRaSender.ino
+++ LoRaSender/LoRaSender.ino
@@ -1,309 +1,66 @@
-/*
-  LoRa Duplex communication
-
-  Sends a message every half second, and polls continually
-  for new incoming messages. Implements a one-byte addressing scheme,
-  with 0xFF as the broadcast address.
-
-  Uses readString() from Stream class to read payload. The Stream class'
-  timeout may affect other functuons, like the radio' callback. For an
-
-  created 28 April 2017
-  by Tom Igoe
-*/
-
 #include <SPI.h>
 #include <LoRa.h>
 #include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-#include <EEPROM.h>
-#include "RTClib.h"
-RTC_DS3231 rtc;
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
-AsyncWebServer server(80);
-AsyncWebSocket ws("/");
-
-const int csPin = 15;    // LoRa radio chip select
-const int resetPin = 16; // LoRa radio reset
-const int irqPin = 12;    // change for your board; must be a hardware interrupt pin
-const int LED = 2;
-const int trigger = 0;
-String timestamp = "";
-String outgoing; // outgoing message
-#define EEPROM_SIZE 12
-byte msgCount = 0;        // count of outgoing messages
-byte localAddress = 0xBB; // address of this device
-byte destination = 0xAA;  // destination to send to
-long lastSendTime = 0;    // last send time
-int interval = 2000;      // interval between sends
-String motorstatus = "off";
-String autoStartMachine = "off";
-int address = 0;
-int addressAutoStart = 1;
-bool isTimerIsRunning = false;
-byte inputDelay=0;
-bool isTimerInititialized = true;
-void initWebSocket()
-{
-  ws.cleanupClients();
-  ws.onEvent(onEvent);
-  server.addHandler(&ws);
-}
-void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
-             void *arg, uint8_t *data, size_t len)
-{
-  if (type == WS_EVT_DATA)
-  {
-    String state = "";
-    for (int i = 0; i < len; ++i)
-    {
-      state = state + String(static_cast<char>(data[i]));
-    }
-    Serial.println(state+"/A "+autoStartMachine+"/M "+motorstatus);
-    if (state == "M=on" && motorstatus == "off")
-    {
-      inputDelay=2;
-      EEPROM.put(address, 1);
-      EEPROM.commit();
-      client->text(state);
-      motorstatus = "on";
-    }
-    else if (state == "M=off" && motorstatus == "on")
-    {
-            inputDelay=2;
-      EEPROM.put(address, 0);
-      EEPROM.commit();
-      client->text(state);
-      motorstatus = "off";
-    }
-    else if (state == "A=on" && autoStartMachine == "off")
-    {
-      EEPROM.put(addressAutoStart, 1);
-      EEPROM.commit();
-      client->text(state);
-      autoStartMachine = "on";
-    }
-    else if (state == "A=off" && autoStartMachine == "on")
-    {
-      EEPROM.put(addressAutoStart, 0);
-      EEPROM.commit();
-      client->text(state);
-      autoStartMachine = "off";
-    }
-    else if (state.endsWith("Z"))
-    {
-      int str_len = state.length() + 1;
-      char char_array[str_len];
-      state.toCharArray(char_array, str_len);
-      rtc.adjust(DateTime(char_array));
-    }
-  }
-  switch (type)
-  {
-  case WS_EVT_CONNECT:
-    client->text("M=" + motorstatus);
-    Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-    client->text("A=" + autoStartMachine);
-    break;
-  case WS_EVT_DISCONNECT:
-    Serial.printf("WebSocket client #%u disconnected\n", client->id());
-    // ws.textAll("N"+String(numberOfClients));
-    break;
-  case WS_EVT_DATA:
-    break;
-  case WS_EVT_PONG:
-    break;
-  case WS_EVT_ERROR:
-    Serial.println("WebSocket ERROR");
-    break;
+#include <WiFiClient.h>
+WiFiServer server(8080);
+WiFiClient client;
+#define ss 15
+#define rst 16
+#define dio0 2
+int LED = 2;
+String state = "1";
+void connectt() {
+  client = server.available();
+  if (client) {
+    Serial.println("Connected to client state == " + state);
+    client.println(state);
   }
 }
-void setup()
-{
-
-  pinMode(trigger, OUTPUT);
-  digitalWrite(trigger, LOW);
+void setup() {
   pinMode(LED, OUTPUT);
   digitalWrite(LED, HIGH);
   Serial.begin(115200);
-  EEPROM.begin(EEPROM_SIZE);
+  Serial.println("LoRa Sender");
+  LoRa.setPins(ss, rst, dio0);
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
   Serial.println();
-  Serial.println("LoRa Duplex");
+  server.begin();
   Serial.print("Setting soft-AP ... ");
   Serial.println(WiFi.softAP("ESP_LORA_S", "bhanu1234") ? "Ready" : "Failed!");
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(myIP);
-  initWebSocket();
-  server.begin();
-  Serial.println();
-  if (!rtc.begin())
-  {
-    Serial.flush();
-    isTimerInititialized = false;
-    digitalWrite(LED, LOW);
-    while (!rtc.begin())
-    {
-      Serial.println("Couldn't find RTC");
-      delay(1000);
-    }
-  }
-  else
-  {
-    digitalWrite(LED, HIGH);
-  }
-
-  if (rtc.lostPower())
-  {
-    Serial.println("RTC lost power, let set the time!");
-    // When time needs to be set on a new device, or after a power loss, the
-    // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2024, 1, 12, 19, 2, 0));
-  }
-  // override the default CS, reset, and IRQ pins (optional)
-
-  LoRa.setPins(csPin, resetPin, irqPin); // set CS, reset, IRQ pin
-  int readId;
-  EEPROM.get(address, readId);
-  if (readId == 0)
-  {
-    motorstatus = "off";
-  }
-  else if (readId == 1)
-  {
-    inputDelay=2;
-    motorstatus = "on";
-  }
-  EEPROM.get(addressAutoStart, readId);
-  if (readId == 0)
-  {
-    autoStartMachine = "off";
-  }
-  else if (readId == 1)
-  {
-    autoStartMachine = "on";
-  }
-  if (!LoRa.begin(433E6))
-  { // initialize ratio at 915 MHz
-    Serial.println("LoRa init failed. Check your connections.");
-    while (true)
-      ; // if failed, do nothing
-  }
-
-  Serial.println("LoRa init succeeded............");
+  connectt();
+}
+void writeToClient(String data) {
+  if (client) client.println(data);
 }
 
-void loop()
-{
-
-  if (millis() - lastSendTime > interval)
+void loop() {
+  Serial.print("Sending packet: ");
+  Serial.println(state);
+  if (!client) {
+    connectt();
+  }
+  else if (client.available())
   {
-    if (isTimerInititialized)
-    {
-      DateTime now = rtc.now();
-      timestamp = String(now.year(), DEC) + '/' + String(now.month(), DEC) + '/' + String(now.day(), DEC) + " T " + String(now.hour(), DEC) + ':' + String(now.minute(), DEC) + ':' + String(now.second(), DEC);
-      Serial.println(timestamp);
-      ws.textAll(timestamp);
+    String dataFromServer = client.readStringUntil('\r');
+    writeToClient(dataFromServer);
+    if (dataFromServer == "A" || dataFromServer == "1") {
+      state = dataFromServer;
+      digitalWrite(LED, LOW );
     }
-    sendMessage(motorstatus + "/" + autoStartMachine);
-    lastSendTime = millis();        // timestamp the message
-    interval = random(2000) + 1000; // 2-3 seconds
-     if(inputDelay!=0)
-    {
-      inputDelay = inputDelay + 2;
+    else if (dataFromServer == "a" || dataFromServer == "0") {
+
+      state = dataFromServer;
+      digitalWrite(LED, HIGH);
     }
+    Serial.println(dataFromServer);
   }
-  else
-  {
-    // display.clear();
-  }
-   if(inputDelay==0)
-  {
-    onReceive(LoRa.parsePacket());
-  }
-  else if(inputDelay>=6)
-  {
-    onReceive(LoRa.parsePacket());
-  }
-  digitalWrite(trigger, motorstatus=="on"?HIGH:LOW);
-}
-
-void sendMessage(String outgoing)
-{
-
-  digitalWrite(LED, HIGH);
-  LoRa.beginPacket();            // start packet
-  LoRa.write(destination);       // add destination address
-  LoRa.write(localAddress);      // add sender address
-  LoRa.write(msgCount);          // add message ID
-  LoRa.write(outgoing.length()); // add payload length
-  LoRa.print(outgoing);          // add payload
-  LoRa.endPacket();              // finish packet and send it
-  msgCount++;
-  Serial.println("Sending " + outgoing);
-}
-
-void onReceive(int packetSize)
-{
-  if (packetSize == 0)
-    return; // if there no packet, return
-inputDelay=0;
-  // read packet header bytes:
-  int recipient = LoRa.read();       // recipient address
-  byte sender = LoRa.read();         // sender address
-  byte incomingMsgId = LoRa.read();  // incoming msg ID
-  byte incomingLength = LoRa.read(); // incoming msg length
-
-  String incoming = "";
-
-  while (LoRa.available())
-  {
-    incoming += (char)LoRa.read();
-  }
-
-  if (incomingLength != incoming.length())
-  { // check length for error
-    Serial.println("error: message length does not match length" + incoming);
-    return; // skip rest of function
-  }
-
-  // if the recipient isn't this device or broadcast,
-  if (recipient != localAddress && recipient != 0xFF)
-  {
-    Serial.println("This message is not for me.");
-    return; // skip rest of function
-  }
-
-  digitalWrite(LED, LOW);
-  // if message is for this device, or broadcast, print details:
-  // Serial.println("Received from: 0x" + String(sender, HEX));
-  // Serial.println("Sent to: 0x" + String(recipient, HEX));
-  // Serial.println("Message ID: " + String(incomingMsgId));
-  // Serial.println("Message length: " + String(incomingLength));
-  Serial.println("Message: " + incoming);
-  Serial.println();
-  if (incoming == "on" && motorstatus == "off")
-  {
-    Serial.println("Called inside");
-    ws.textAll("M=on");
-    motorstatus = "on";
-    EEPROM.put(address, 1);
-    EEPROM.commit();
-     
-  }
-  else if (incoming == "off" && motorstatus == "on")
-  {
-    Serial.println("Called inside");
-    ws.textAll("M=off");
-    motorstatus = "off";
-    EEPROM.put(address, 0);
-    EEPROM.commit();
-    
-  }
-  Serial.println();
+  LoRa.beginPacket();
+  LoRa.print(state);
+  LoRa.endPacket();
 }
--- RS02Receiver/RS02Receiver.ino
+++ RS02Receiver/RS02Receiver.ino
@@ -1,261 +1,79 @@
-/*
-  LoRa Duplex communication
-
-  Sends a message every half second, and polls continually
-  for new incoming messages. Implements a one-byte addressing scheme,
-  with 0xFF as the broadcast address.
-
-  Uses readString() from Stream class to read payload. The Stream class'
-  timeout may affect other functuons, like the radio's callback. For an
-
-  created 28 April 2017
-  by Tom Igoe
-*/
-#include <SPI.h>  // include libraries
+#include <SPI.h>
 #include <LoRa.h>
 #include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-#include <EEPROM.h>
-#define EEPROM_SIZE 12
-const int csPin = 15;     // LoRa radio chip select
-const int resetPin = 16;  // LoRa radio reset
-const int irqPin = 12;
-const int motorLed = 0;  // change for your board; must be a hardware interrupt pin
-const int LED = 2;
-const int motorRelay = 4;
-const int switchinput = 5;
-String outgoing;  // outgoing message
-String motorstatus = "off";
-String autoStartMachine = "off";
-byte msgCount = 0;         // count of outgoing messages
-byte localAddress = 0xAA;  // address of this device
-byte destination = 0xBB;   // destination to send to
-long lastSendTime = 0;     // last send time
-int interval = 2000;
-bool clicked = false;  // interval between sends
-bool canISend = true;
-AsyncWebServer server(80);
-AsyncWebSocket ws("/");
-int address = 0;
-byte inputDelay = 0;
-void initWebSocket() {
-  ws.cleanupClients();
-  ws.onEvent(onEvent);
-  server.addHandler(&ws);
-}
-void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
-             void *arg, uint8_t *data, size_t len) {
-  if (type == WS_EVT_DATA) {
-    String state = "";
-    for (int i = 0; i < len; ++i) {
-      state = state + String(static_cast<char>(data[i]));
-    }
-    Serial.println(state);
-    if (state == "M=on" && motorstatus == "off") {
-      inputDelay = 2;
-      // EEPROM.put(address, 1);
-      // EEPROM.commit();
-      client->text(state);
-      motorstatus = "on";
-    } else if (state == "M=off" && motorstatus == "on") {
-      inputDelay = 2;
-      // EEPROM.put(address, 0);
-      // EEPROM.commit();
-      client->text(state);
-      motorstatus = "off";
-    }
+#include <WiFiClient.h>
+WiFiServer server(8080);
+WiFiClient client;
+ String state = "0";
+int LED = 2;   //d4
+int d1=5;
+#define ss 15
+#define rst 16
+#define dio0 2
+bool isClient = false;
+void connectt() {
+  client = server.available();
+  if (client) { 
+  if (digitalRead(LED)==HIGH) {
+    client.println("a");
   }
-  switch (type) {
-    case WS_EVT_CONNECT:
-      client->text("M=" + motorstatus);
-      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-      client->text("A=" + autoStartMachine);
-      break;
-    case WS_EVT_DISCONNECT:
-      Serial.printf("WebSocket client #%u disconnected\n", client->id());
-      // ws.textAll("N"+String(numberOfClients));
-      break;
-    case WS_EVT_DATA:
-      break;
-    case WS_EVT_PONG:
-      break;
-    case WS_EVT_ERROR:
-      Serial.println("WebSocket ERROR");
-      break;
+  else  {
+     client.println("A");
+  } 
+   Serial.println("Connected to client");
   }
+     
+     
 }
-
-void setup() {
+void setup() { 
   pinMode(LED, OUTPUT);
   digitalWrite(LED, HIGH);
-   pinMode(motorRelay, OUTPUT);
-  digitalWrite(motorRelay, LOW);
-  pinMode(motorLed, OUTPUT);
-  digitalWrite(motorLed, LOW);
-  pinMode(switchinput, INPUT);
-  Serial.begin(115200);
-  EEPROM.begin(EEPROM_SIZE);  // initialize serial
-  Serial.println("LoRa Duplex RECEIVED");
-  int readId;
-  EEPROM.get(address, readId);
-  if (readId == 0) {
-    autoStartMachine = "off";
-    motorstatus = "off";
-  } else if (readId == 1) {
-    inputDelay = 2;
-    autoStartMachine = "on";
-    motorstatus = "on";
+    pinMode(d1, OUTPUT);
+  digitalWrite(d1, LOW);
+  Serial.begin(115200);  
+  Serial.println("LoRa Receiver");
+LoRa.setPins(ss, rst, dio0);
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
   }
-
-  // override the default CS, reset, and IRQ pins (optional)
-  LoRa.setPins(csPin, resetPin, irqPin);  // set CS, reset, IRQ pin
-
-  if (!LoRa.begin(433E6)) {  // initialize ratio at 915 MHz
-    Serial.println("LoRa init failed. Check your connections.");
-    while (true)
-      ;  // if failed, do nothing
-  }
-  Serial.println("LoRa Duplex");
+  Serial.println();
+  server.begin();
   Serial.print("Setting soft-AP ... ");
   Serial.println(WiFi.softAP("ESP_LORA_R", "bhanu1234") ? "Ready" : "Failed!");
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(myIP);
-  initWebSocket();
-  server.begin();
-  Serial.println("LoRa init succeeded.");
+  connectt();
 }
-
-void loop() {
-  if (digitalRead(switchinput) == LOW && clicked == false) {
-    clicked = true;
-    inputDelay = 2;
-    if (motorstatus == "on") {
-      sendMessage("off");
-      motorstatus = "off";
-      digitalWrite(motorLed, LOW);
-    } else {
-      sendMessage("on");
-      motorstatus = "on";
-      digitalWrite(motorLed, HIGH);
-    }
-    lastSendTime = millis();  // timestamp the message
-    interval = random(2000) + 1000;
-  } else if (digitalRead(switchinput) == HIGH) {
-    clicked = false;
+void writeToClient(String data1) {
+    if (data1 == "A" || data1 == "1") {
+       state = data1;
+    digitalWrite(LED, LOW );
+    digitalWrite(d1, HIGH);
   }
-
-  if (millis() - lastSendTime > interval) {
+  else if (data1 == "a" || data1 == "0") {
     digitalWrite(LED, HIGH);
-    sendMessage(motorstatus);
-    Serial.println("Sending " + motorstatus);
-    lastSendTime = millis();         // timestamp the message
-    interval = random(2000) + 1000;  // 2-3 seconds
-    if(inputDelay!=0)
-    {
-      inputDelay = inputDelay + 2;
-    }
+      digitalWrite(d1, LOW);
+      state = data1;
   }
-  if (motorstatus == "on") {
-    digitalWrite(motorLed, HIGH);
-    digitalWrite(motorRelay, HIGH);
-  } else {
-    digitalWrite(motorLed, LOW);
-    digitalWrite(motorRelay, LOW);
-  }
-  // parse for a packet, and call onReceive with the result:
-  if (inputDelay == 0) {
-    onReceive(LoRa.parsePacket());
-  } else if (inputDelay >= 6) {
-    onReceive(LoRa.parsePacket());
-  }
+ if (client) 
+  { 
+  client.println(data1);
+  } 
+  Serial.println(data1);
 }
-
-void sendMessage(String outgoing) {
-  LoRa.beginPacket();             // start packet
-  LoRa.write(destination);        // add destination address
-  LoRa.write(localAddress);       // add sender address
-  LoRa.write(msgCount);           // add message ID
-  LoRa.write(outgoing.length());  // add payload length
-  LoRa.print(outgoing);           // add payload
-  LoRa.endPacket();               // finish packet and send it
-  msgCount++;                     // increment message ID
-}
-
-void onReceive(int packetSize) {
-  if (packetSize == 0) return;  // if there's no packet, return
-  inputDelay = 0;
-  // read packet header bytes:
-  int recipient = LoRa.read();        // recipient address
-  byte sender = LoRa.read();          // sender address
-  byte incomingMsgId = LoRa.read();   // incoming msg ID
-  byte incomingLength = LoRa.read();  // incoming msg length
-
-  String incoming = "";
-  digitalWrite(LED, LOW);
-  while (LoRa.available()) {
-    incoming += (char)LoRa.read();
-  }
-  if (incoming == "on" || incoming == "off") {
-    motorstatus = incoming;
-    canISend = false;
-  }
-
-  if (incomingLength != incoming.length()) {  // check length for error
-    Serial.println("error: message length does not match length" + incoming);
-    return;  // skip rest of function
-  }
-
-  // if the recipient isn't this device or broadcast,
-  if (recipient != localAddress && recipient != destination) {
-    Serial.println("This message is not for me.");
-    return;  // skip rest of function
-  }
-
-  int index = incoming.indexOf("/");
-  String incomingMotorState = "";
-  String incomingAutoStartMotorState = "";
-  if (index > -1) {
-    for (int i = 0; i < index; i++) {
-      incomingMotorState = incomingMotorState + (incoming.charAt(i));
-    }
-    if (incomingMotorState != "") {
-
-      if (incomingMotorState == "on" && motorstatus == "off") {
-        ws.textAll("M=on");
-        motorstatus = "on";
-      } else if (incomingMotorState == "off" && motorstatus == "on") {
-        ws.textAll("M=off");
-        motorstatus = "off";
-      }
-    }
-    for (int i = index + 1; i < incoming.length(); i++) {
-      incomingAutoStartMotorState = incomingAutoStartMotorState + (incoming.charAt(i));
-    }
-    if (incomingAutoStartMotorState != "") {
-
-      if (incomingAutoStartMotorState == "on" && autoStartMachine == "off") {
-        EEPROM.put(address, 1);
-        EEPROM.commit();
-        autoStartMachine = "on";
-        ws.textAll("A=on");
-      } else if (incomingAutoStartMotorState == "off" && autoStartMachine == "on") {
-        EEPROM.put(address, 0);
-        EEPROM.commit();
-        autoStartMachine = "off";
-        ws.textAll("M=off");
-      }
-    }
-  }
-  // if message is for this device, or broadcast, print details:
-  // Serial.println("Received from: 0x" + String(sender, HEX));
-  // Serial.println("Sent to: 0x" + String(recipient, HEX));
-  // Serial.println("Message ID: " + String(incomingMsgId));
-  // Serial.println("Message length: " + String(incomingLength));
-  Serial.println("Message: " + incoming);
-
-  // Serial.println("RSSI: " + String(LoRa.packetRssi()));
-  // Serial.println("Snr: " + String(LoRa.packetSnr()));
-  Serial.println();
+ 
+void loop() {
+  // try to parse packet
+   if (!client) {
+    connectt();
+  } 
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) { 
+    while (LoRa.available()) {
+      String c=LoRa.readString(); 
+        writeToClient(c);
+    } 
+  } 
 }
